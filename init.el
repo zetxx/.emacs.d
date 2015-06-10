e37b67c72392ac1c3a3023122f268c103561775c
zetxx <opensuser@gmail.com>
2015-06-11 00:12:12 +0300

    home goes to firrst printable char

--- init.el
+++ init.el
@@ -157,6 +157,20 @@
 
 (add-hook 'minibuffer-setup-hook 'my-minibuffer-setup-hook)
 ;; key remap minor mode end
+
+(defun smart-beginning-of-line ()
+  "Move point to first non-whitespace character or beginning-of-line.
+
+Move point to the first non-whitespace character on this line.
+If point was already at that position, move point to beginning of line."
+  (interactive) ; Use (interactive "^") in Emacs 23 to make shift-select work
+  (let ((oldpos (point)))
+    (back-to-indentation)
+    (and (= oldpos (point))
+         (beginning-of-line))))
+
+(global-set-key [home] 'smart-beginning-of-line)
+
 ;; paste and format
 (dolist (command '(yank yank-pop))
    (eval `(defadvice ,command (after indent-region activate)
